517a4fef837774618e2e9dcf1b4e11adfc0d567b
Michael Grimmer <nolia13@hotmail.com>
2021-03-31 23:46:22 +1100

    Initial Commit

---
+++ Market Stream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Market Stream
+//
+//  Created by Michael Grimmer on 31/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
