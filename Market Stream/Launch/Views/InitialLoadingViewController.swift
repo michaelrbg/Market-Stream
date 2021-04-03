d6e466620f6e061d3606918af708915738d3abb8
Michael Grimmer <nolia13@hotmail.com>
2021-04-03 22:33:06 +1100

    [MS-2] Deleted the Main.storyboard and ViewController.swift files. Deleted the "UISceneStoryboardFile" key-value pair and the "UIMainStoryboardFile" key-value pair from the Info.plist file.
    Created the InitialLoadingViewController and changed the scene(... willConnectTo ...) function (in the SceneDelegate) so that it now assigns the InitialLoadingViewController as the rootViewController.

---
+++ Market Stream/Launch/Views/InitialLoadingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InitialLoadingViewController.swift
+//  Market Stream
+//
+//  Created by Michael Grimmer on 2/4/21.
+//
+
+import UIKit
+
+final class InitialLoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
