4b06e84a2cbdfe2739508df92b344eaf8227961d
Michael Grimmer <nolia13@hotmail.com>
2021-04-07 11:55:54 +1000

    [MS-3] Created the UserSessionServiceType. Added an AppDelegate extension with a call to the new .createSessionIfNeeded protocol function.

---
+++ Market Stream/MarketStreamDomain/User/Services/UserSessionServiceType.swift
@@ -0,0 +1,10 @@
+//
+//  UserSessionServiceType.swift
+//  Market Stream
+//
+//  Created by Michael Grimmer on 5/4/21.
+//
+
+protocol UserSessionServiceType {
+    static func createSessionIfNeeded()
+}
